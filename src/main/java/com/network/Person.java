82e55513e4a2e1f0a70e9deaa0310a88d7005bef
Maxim Finozhenok <finojenok.m@gmail.com>
2020-03-23 20:46:22 +0300

    Add class Page and include it to class Person

--- src/main/java/com/network/Person.java
+++ src/main/java/com/network/Person.java
@@ -1,17 +1,26 @@
 package main.java.com.network;
 
 public class Person {
-    String name = "undefined";
+    String name = "undefined"; // Name of person
+    Page page; //Page of person
 
     Person(){}
 
     public Person(String name){
         this.name=name;
+        this.page = new Page();
+    }
+
+    public Person(String name, int color){
+        this.name = name;
+        this.page=new Page(color);
     }
 
     public String getName(){
         return name;
     }
+
+    public int getColor(){ return page.color }
 
 
 

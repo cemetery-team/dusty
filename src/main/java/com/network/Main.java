890d48cd280b34064ddfb46b81684ce477c26754
Maxim Finozhenok <finojenok.m@gmail.com>
2020-05-03 20:02:43 +0300

    mod Main

--- src/main/java/com/network/Main.java
+++ src/main/java/com/network/Main.java
@@ -1,45 +1,16 @@
 package com.network;
 
-/*
-import com.mongodb.Block;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import main.java.com.network.Person;
-import org.bson.Document;
-*/
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Main.class, args);
-
-        /*
-        String server = "3.8.161.24";
-
-        Person person = new Person("Charles", 2);
-
-        MongoClient mongoClient = MongoClients.create("mongodb://" + server + ":27017");
-        MongoDatabase database = mongoClient.getDatabase("dustydb");
-        MongoCollection<Document> collection = database.getCollection("persons");
-
-        System.out.println(database.getName());
-
-        Block<Document> printBlock = new Block<Document>() {
-            @Override
-            public void apply(Document document) {
-                System.out.println(document.toJson());
-            }
-        };
-
-        collection.find().forEach(printBlock);
-        */
     }
 
 

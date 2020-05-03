d7f7e7826fa71dc0d46b61ca4cb61a469e0b39ea
Maxim Finozhenok <finojenok.m@gmail.com>
2020-05-03 21:15:06 +0300

    mapped /

--- src/main/java/com/network/Group.java
+++ src/main/java/com/network/Group.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Group {
-    @GetMapping("/group")
+    @GetMapping("/")
     public String group(){
         return "group";
     }

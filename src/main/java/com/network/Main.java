60c60629f254a58013d011b897a6a427ec2e9024
Maxim Finozhenok <finojenok.m@gmail.com>
2020-11-06 00:53:49 +0300

    migate to HttpServer

--- src/main/java/com/network/Main.java
+++ src/main/java/com/network/Main.java
@@ -1,16 +1,11 @@
 package com.network;
 
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-
-@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+
+
     }
 
 

f621473bfb178968c60b5f96434fa6a5a3cbb311
norwegianblackmetal <finojenok.m@gmail.com>
2020-11-06 01:44:24 +0300

    migrate

--- src/main/java/com/network/Main.java
+++ src/main/java/com/network/Main.java
@@ -1,12 +1,31 @@
 package com.network;
+import com.sun.net.httpserver.*;
+import org.springframework.http.HttpRequest;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpCookie;
+import java.net.InetSocketAddress;
+import java.net.http.HttpResponse;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create();
+        server.bind(new InetSocketAddress(8081), 1);
+        HttpContext context = server.createContext("/", new ResHandler());
+        server.start();
     }
 
 
+    private static class ResHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            httpExchange.sendResponseHeaders(200, "metallica".length());
+            OutputStream os = httpExchange.getResponseBody();
+            os.write("metallica".getBytes());
+            os.close();
+
+        }
+    }
 }
